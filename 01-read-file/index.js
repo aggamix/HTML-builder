4fd33f47b123143106bcf06a75293690c15cd270
Mikita <nikitakuza1999@gmail.com>
2024-01-21 17:37:48 +0100

    init: start HTML-builder task; feat: add solution for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const readStream = fs.createReadStream(
+  path.resolve(__dirname, 'text.txt'),
+  'utf-8',
+);
+readStream.on('data', (data) => console.log(data));
+readStream.on('error', (error) => console.log('Error: ' + error.message));
