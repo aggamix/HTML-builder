c5e40f9143df9d9a2debe77c59930023947b2401
Mikita <nikitakuza1999@gmail.com>
2024-01-21 21:44:57 +0100

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Hi, write your text...\n');
+stdin.on('data', (data) => {
+  const text = data.toString();
+  if (text.includes('exit')) {
+    console.log('Thx, Have a good day!');
+    process.exit();
+  }
+  writeStream.write(text);
+});
+
+process.on('SIGINT', () => {
+  stdout.write('Have a good day!\n');
+  process.exit();
+});
